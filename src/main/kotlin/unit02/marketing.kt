4ce7dd652ae3032b81b424f1a5b409fe7c6896c2
Fazzught <carnoult1280@gmail.com>
2023-02-12 22:39:12 -0600

    Finished marketing for unit 02.3

---
+++ src/main/kotlin/unit02/marketing.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var friendsCount = 626
+    if (friendsCount < 500) {
+        println ("Buy Cat Food At 20% Off.")
+    }
+    else {
+        println ("Adopt a Dog Today!")
+    }
+}
