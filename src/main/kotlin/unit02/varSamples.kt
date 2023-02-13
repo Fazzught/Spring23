2fd31061fe22baab966ea607723523001eddb412
Fazzught <carnoult1280@gmail.com>
2023-02-12 21:53:13 -0600

    Finished variable for unit 02.2

---
+++ src/main/kotlin/unit02/varSamples.kt
@@ -0,0 +1,14 @@
+fun main(){
+
+    var name = "Christian"
+    var num = 13
+    var numLetter = "12"
+
+    println(name)
+    println(num)
+    println(numLetter)
+
+    println("Your number times 2 is " + num*2)
+    println("The other number times 2 is " + (num-1)*2)
+
+}
---
+++ src/main/kotlin/unit02/variables.kt
@@ -0,0 +1,17 @@
+fun main(){
+    val myAge: Int = 33
+
+    var averageAge: Double = 33.0
+    averageAge = (33.0 + 30.0)/2
+
+    val testNumber: Int = 21
+    val evenOdd = testNumber % 2
+    // testNumber cannot be changed because it's a constant
+
+    var answer = 0
+    answer ++
+    answer +=10
+    answer *=10
+    answer *=1000
+    //After all of these operations answer is equal to 110000
+}
