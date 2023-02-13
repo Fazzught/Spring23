0b7621f573c7a5efe897830ad2f4383ddd56654f
Fazzught <carnoult1280@gmail.com>
2023-02-12 22:51:56 -0600

    Finished marketing for unit 02.3

---
+++ src/main/kotlin/unit02/salary.kt
@@ -0,0 +1,7 @@
+fun main (){
+
+    var name: String = "Christian Arnoult"
+    var age: Int = 33
+    var annualPay: Double = 100000.00
+    println ("My name is $name, my age is $age and I hope to earn $$annualPay per year.")
+}
