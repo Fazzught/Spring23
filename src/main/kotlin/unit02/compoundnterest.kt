12278f785c0aa325ce9f5985182ec3054604fe78
Fazzught <carnoult1280@gmail.com>
2023-02-14 00:45:17 -0600

    Finished marketing for unit 02.5

---
+++ src/main/kotlin/unit02/compoundnterest.kt
@@ -0,0 +1,24 @@
+import kotlin.math.*
+
+fun main() {
+    print("Enter the principal amount of money deposited: ")
+    var principal = readLine()!!
+    print("Enter the annual interest rate as a percent value: ")
+    var interest = readLine()!!
+    print("Enter the number of times per year interest is compounded: ")
+    var compounded = readLine()!!
+    print("Enter the number of years: ")
+    var years = readLine()!!
+
+    var p = principal.toDouble()
+    var i = (interest.toDouble() / 100)
+    var c = compounded.toDouble()
+    var y = years.toDouble()
+    var compPerYear = c * y
+    var x = (1 + (i / c))
+    var amount = p * Math.pow(x.toDouble(), compPerYear.toDouble())
+    var amountRounded = String.format("%.2f", amount)
+
+    println("The amount is equal to $$amountRounded.")
+
+}
