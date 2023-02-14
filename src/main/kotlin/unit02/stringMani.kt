64df7026454732b7d2098d74c9711ec2505c6227
Fazzught <carnoult1280@gmail.com>
2023-02-13 23:47:40 -0600

    Finished marketing for unit 02.5

--- src/main/kotlin/unit02/stringMani.kt
+++ src/main/kotlin/unit02/stringMani.kt
@@ -1,8 +1,8 @@
 fun main(args: Array<String>) {
-    print("Enter the name of you're favorite city: ")
-    var city = readLine()!!
-    println(city.length)
-    println(city.toUpperCase())
-    println(city.toLowerCase())
-    println(city[0])
+    print("Enter the name of your favorite city: ")
+    var cityName = readLine()!!
+    println(cityName.length)
+    println(cityName.toUpperCase())
+    println(cityName.toLowerCase())
+    println(cityName[0])
 }
