a9b2f0fee377b642cf653be78f96f7d8d29d3322
Fazzught <carnoult1280@gmail.com>
2023-04-25 17:01:08 -0500

    commiting 5.1

---
+++ src/main/kotlin/unit05/primeTime.kt
@@ -0,0 +1,20 @@
+fun isNumberDivisible (number: Int, divisor: Int): Boolean {
+    return number % divisor == 0
+}
+fun isPrime(number: Int): Boolean {
+    if (number <=1) {
+        return false
+    }
+    for (i in 2 until number){
+        if (isNumberDivisible(number, i)){
+            return false
+        }
+    }
+    return true
+}
+
+fun main(){
+    println(isPrime(6))
+    println(isPrime(13))
+    println(isPrime(8893))
+}
